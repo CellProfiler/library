2272d3cf63e031b1b099f1c59f134fa2ce70f168
bethac07 <bcimini@broadinstitute.org>
2022-02-15 13:53:16 -0500

    first library module

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+setuptools.setup(
+    author="Beth Cimini",
+    author_email="bcimini@broadinstitute.org",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    extras_require={
+        "dev": [
+            "black==19.10b0",
+            "click>=7.1.2",
+            "pre-commit==2.2.0",
+            "sphinx==3.1.2",
+            "twine==3.1.1",
+        ],
+        "test": ["pytest==5.4.1"],
+    },
+    install_requires=[
+        "centrosome==1.2.0",
+        "mahotas>=1.4",
+        "matplotlib==3.1.3",
+        "mysqlclient==1.4.6",
+        "numpy>=1.20.1",
+        "scikit-image>=0.17.2",
+        "scikit-learn>=0.20",
+        "scipy>=1.4.1",
+    ],
+    license="BSD",
+    name="cellprofiler-library",
+    package_data={"cellprofiler_library": ["py.typed"]},
+    packages=setuptools.find_packages(exclude=["tests"]),
+    python_requires=">=3.8",
+    url="https://github.com/CellProfiler/library",
+    version="0.0.0",
+    zip_safe=False,
+)
